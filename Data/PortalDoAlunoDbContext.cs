b6e6afd34f9f89b911bb19afcac2a13ee15be5d5
Marcelo Victor <marceloblvictor@gmail.com>
2021-01-01 19:21:08 -0300

    Add project files.

---
+++ Data/PortalDoAlunoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalDoAluno.Model;
+
+namespace PortalDoAluno.Data
+{
+    /// <summary>
+    /// Contexto da sessão do Entity Framework com o Banco de Dados da aplicação Portal do Aluno
+    /// </summary>
+    public class PortalDoAlunoDbContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
