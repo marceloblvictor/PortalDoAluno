a7d25fb74bc5cba873b3e5d3d769f03bea1643fb
Marcelo Victor <marceloblvictor@gmail.com>
2021-01-22 21:05:18 -0300

    Acrescenta a propriedade Price em Course

--- Data/PortalDoAlunoDbContext.cs
+++ Data/PortalDoAlunoDbContext.cs
@@ -16,8 +16,15 @@ namespace PortalDoAluno.Data
             // Creates entities tables with names in singular, overriding the default behavior of using the DbSet name
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Student>().ToTable("Student");
+
+            // Configures the relationship delete to Restrict. By convention, the Entity Framework enables cascade 
+            // delete for non-nullable foreign keys and for many-to-many relationships
+            //modelBuilder.Entity<Department>()
+            //    .HasOne(d => d.Administrator)
+            //    .WithMany()
+            //    .OnDelete(DeleteBehavior.Restrict);
         }
-        
+
         public DbSet<Student> Students { get; set; }
         
         public DbSet<Course> Courses { get; set; }
