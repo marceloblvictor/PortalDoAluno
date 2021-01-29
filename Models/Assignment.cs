60d4833508b894ee2509ca1bafc92de352f8c595
Marcelo Victor <marceloblvictor@gmail.com>
2021-01-28 22:17:32 -0300

    Cria um projeto para testes unitários

---
+++ Models/Assignment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalDoAluno.Models
+{
+    public class Assignment
+    {
+        public int CourseID { get; set; }
+    }
+}
